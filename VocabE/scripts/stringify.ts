9d22139000215223bb0124ded23e9e9261082e3c
KhanhGia-HaUI <115928934+KhanhGia-HaUI@users.noreply.github.com>
2023-01-27 19:36:55 +0700

    Delete VocabE directory

--- VocabE/scripts/stringify.ts
+++
@@ -1,4 +0,0 @@
-"use strict";
-export default function(obj: object){
-    return JSON.stringify(obj, null, '\t');
-}
--- VocabE/scripts/timer.ts
+++
@@ -1,7 +0,0 @@
-"use strict";
-export default async function(callback) {
-    const before : number = Date.now();
-    await callback().finally(result => result);
-    const after : number = Date.now(); 
-    return (after - before)/1000;
-}
